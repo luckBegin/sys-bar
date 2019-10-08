c307a6d9c301dde10121c31d479286db025fd69e
sirius.black <811140213@qq.com>
2019-10-08 20:36:32 +0800

    [增加]房台列表实体

---
+++ api/src/module/room/list/entities/list.entity.ts
@@ -0,0 +1,32 @@
+import { Entity, Column } from 'typeorm' ;
+import { ApiModelProperty } from '@nestjs/swagger' ;
+import { BasicEntity } from '../../../../share/entities' ;
+
+@Entity()
+export class Room_type extends BasicEntity {
+	@Column()
+	@ApiModelProperty({description: '店铺ID'})
+	shopId: number ;
+
+	@Column()
+	@ApiModelProperty({description: '区域名称' })
+	name: string ;
+
+	@Column()
+	@ApiModelProperty({description:'备注'})
+	remark: string ;
+}
+
+export interface  IQuery {
+	shopId: number ;
+	id: number ;
+}
+
+export class Query implements IQuery{
+	public shopId: number ;
+	public id: number ;
+	constructor( query: { [key: string]: any }) {
+		if( query.shopId )
+			this.shopId = query.shopId;
+	}
+}
